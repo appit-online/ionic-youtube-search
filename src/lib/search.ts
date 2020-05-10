793c0edb21010b0674d2b3c3217d30799dcd5599
David Koller <davidkoller7117+github@gmail.com>
2020-05-10 14:53:04 +0200

    -fix api

--- src/lib/search.ts
+++ src/lib/search.ts
@@ -9,7 +9,6 @@ export async function searchVideo(searchString: string) {
   const searchValue = await searchRes.data;
   const pattern = new RegExp('initial-data"><!-- {.*--></div><script >');
   const matches = searchValue.match(pattern);
-
   let content = matches[0];
   content = content.replace('initial-data"><!-- ', '');
   content = content.replace('--></div><script >', '');
@@ -20,33 +19,35 @@ export async function searchVideo(searchString: string) {
   const videos = [];
   for (let result of contentObject[0].itemSectionRenderer.contents) {
     try{
-      result = result.compactVideoRenderer;
+      if(result.compactVideoRenderer !== undefined){
+        result = result.compactVideoRenderer;
 
-      const videoId = result.videoId;
-      const url = YOUTUBE_URL + '/watch?v=' + videoId;
-      const duration = result.lengthText;
-      const thumbnailURLs = result.thumbnail.thumbnails;
-      const publishedAt = result.publishedTimeText.runs[0].text;
-      const title = result.title.runs[0].text;
-      const views = result.viewCountText;
+        const videoId = result.videoId;
+        const url = YOUTUBE_URL + '/watch?v=' + videoId;
+        const duration = result.lengthText;
+        const thumbnailURLs = result.thumbnail.thumbnails;
+        const publishedAt = result.publishedTimeText.runs[0].text;
+        const title = result.title.runs[0].text;
+        const views = result.viewCountText;
 
-      videos.push({
-        id: {
-          videoId
-        },
-        snippet: {
-          url,
-          publishedAt,
-          thumbnails: {default: thumbnailURLs[0], high: thumbnailURLs[thumbnailURLs.length - 1]},
-          duration,
-          title,
-          views,
-        }
-      });
+        videos.push({
+          id: {
+            videoId
+          },
+          snippet: {
+            url,
+            publishedAt,
+            thumbnails: {default: thumbnailURLs[0], high: thumbnailURLs[thumbnailURLs.length - 1]},
+            duration,
+            title,
+            views,
+          }
+        });
+      }
     }catch(error){
       // tslint:disable-next-line:no-console
       console.log(error);
     }
-    return videos;
   }
+  return videos;
 }
